f6c7b5d697f683eeeb4d2bb5e81ed1b1360b931f
jeffdatura <stramoine7@gmail.com>
2022-08-02 21:04:26 +0000

    Update Employes.js

--- models/Employes.js
+++ models/Employes.js
@@ -1,11 +1,11 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var EmployeeSchema = new mongoose.Schema({
-    name: String,
-    address: String,
-    position: String,
-    salary: Number,
-    updated_at: { type: Date, default: Date.now },
+  name: String,
+  address: String,
+  position: String,
+  salary: Number,
+  updated_at: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Employee', EmployeeSchema);
+module.exports = mongoose.model("Employee", EmployeeSchema);
